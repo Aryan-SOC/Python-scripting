adf2bb76dfb7c5df4c4626524ed36be1e988044b
Shewag Bhattarai <bhattaraishewag178@gmail.com>
2025-04-12 13:27:11 +0530

    first python

---
+++ practice_01.py
@@ -0,0 +1,13 @@
+print("[Recon in progress...]")
+domain_name = "cybersonick.in"
+ip = "192.168.1.1"
+server_status = "Online"
+open_ports = "22, 80, 443, 8080"
+known_services = "SSH, HTTP, HTTPS, HTTP Proxy"
+known_vulnerabilities = "None"
+print(f"Domain Name: {domain_name}")
+print(f"IP Address: {ip}")
+print(f"Server Status: {server_status}")
+print(f"Open Ports: {open_ports}")
+print(f"Known Services: {known_services}")
+print(f"Known Vulnerabilities: {known_vulnerabilities}")
